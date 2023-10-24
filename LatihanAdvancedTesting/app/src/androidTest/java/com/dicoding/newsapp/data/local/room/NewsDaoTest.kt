b1516646dde77db926f1f82ea8b5d2dc93475097
Abdullah Hali Saputra <abdullahhali060@gmail.com>
2023-10-24 19:39:59 +0700

    practice advanced testing

--- LatihanAdvancedTesting/app/src/androidTest/java/com/dicoding/newsapp/data/local/room/NewsDaoTest.kt
+++ LatihanAdvancedTesting/app/src/androidTest/java/com/dicoding/newsapp/data/local/room/NewsDaoTest.kt
@@ -1,5 +1,55 @@
 package com.dicoding.newsapp.data.local.room
 
-import org.junit.Assert.*
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import com.dicoding.newsapp.utils.DataDummy
+import com.dicoding.newsapp.utils.getOrAwaitValue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 
-class NewsDaoTest
+@Suppress("OPT_IN_IS_NOT_ENABLED")
+@OptIn(ExperimentalCoroutinesApi::class)
+class NewsDaoTest {
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var database: NewsDatabase
+    private lateinit var dao: NewsDao
+
+    private val sampleNews = DataDummy.generateDummyNewsEntity()[0]
+
+    @Before
+    fun initDb() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            NewsDatabase::class.java
+        ).build()
+        dao = database.newsDao()
+    }
+
+    @After
+    fun closeDb() = database.close()
+
+    @Test
+    fun saveNews_Success() = runTest {
+        dao.saveNews(sampleNews)
+        val actualNews = dao.getBookmarkedNews().getOrAwaitValue()
+        Assert.assertEquals(sampleNews.title, actualNews[0].title)
+        Assert.assertTrue(dao.isNewsBookmarked(sampleNews.title).getOrAwaitValue())
+    }
+
+    @Test
+    fun deleteNews_Success() = runTest {
+        dao.saveNews(sampleNews)
+        dao.deleteNews(sampleNews.title)
+        val actualNews = dao.getBookmarkedNews().getOrAwaitValue()
+        Assert.assertTrue(actualNews.isEmpty())
+        Assert.assertFalse(dao.isNewsBookmarked(sampleNews.title).getOrAwaitValue())
+    }
+}
--- LatihanAdvancedTesting/app/src/test/java/com/dicoding/newsapp/ui/list/NewsViewModelTest.kt
+++ LatihanAdvancedTesting/app/src/test/java/com/dicoding/newsapp/ui/list/NewsViewModelTest.kt
@@ -1,5 +1,59 @@
 package com.dicoding.newsapp.ui.list
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.dicoding.newsapp.data.NewsRepository
+import com.dicoding.newsapp.data.Result
+import com.dicoding.newsapp.data.local.entity.NewsEntity
+import com.dicoding.newsapp.ui.utils.DataDummy
+import com.dicoding.newsapp.ui.utils.getOrAwaitValue
+import org.junit.Assert
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
 
-class NewsViewModelTest
+@RunWith(MockitoJUnitRunner::class)
+class NewsViewModelTest{
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var newsRepository: NewsRepository
+    private lateinit var newsViewModel: NewsViewModel
+    private val dummyNews = DataDummy.generateDummyNewsEntity()
+
+    @Before
+    fun setUp() {
+        newsViewModel = NewsViewModel(newsRepository)
+    }
+
+    @Test
+    fun `when Get HeadLineNews Should Not Null and Return Success`(){
+        val observer = Observer<Result<List<NewsEntity>>>{}
+        val expectedNews = MutableLiveData<Result<List<NewsEntity>>>()
+        expectedNews.value = Result.Success(dummyNews)
+        `when` (newsRepository.getHeadlineNews()).thenReturn(expectedNews)
+        val actualNews = newsViewModel.getHeadlineNews().getOrAwaitValue()
+        Assert.assertNotNull(actualNews)
+        Assert.assertTrue(actualNews is Result.Success)
+        Assert.assertEquals(dummyNews.size, (actualNews as Result.Success).data.size)
+    }
+
+    @Test
+    fun `when Network Error Should Return Error`() {
+        val headLineNews = MutableLiveData<Result<List<NewsEntity>>>()
+        headLineNews.value = Result.Error("Error")
+        `when`(newsRepository.getHeadlineNews()).thenReturn(headLineNews)
+        val actualNews = newsViewModel.getHeadlineNews().getOrAwaitValue()
+        Mockito.verify(newsRepository).getHeadlineNews()
+        Assert.assertNotNull(actualNews)
+        Assert.assertTrue(actualNews is Result.Error)
+    }
+}
